a906811849d6f04b34264983357d93cb21b79743
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-09-05 16:49:22 +0700

    Updating project.

--- app/src/main/java/com/taufik/tixidgithubuser/data/api/UrlEndPoint.kt
+++ app/src/main/java/com/taufik/tixidgithubuser/data/api/UrlEndPoint.kt
@@ -4,7 +4,7 @@ object UrlEndPoint {
 
     const val BASE_URL = "https://api.github.com/"
     const val USERS_PARAM = "users"
-    const val API_KEY = "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"
+    const val API_KEY = ""
     const val STARTING_PAGE_INDEX = 1
     const val PER_PAGE = 10
 }
