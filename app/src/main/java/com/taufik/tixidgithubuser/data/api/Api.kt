62b37ea70a7d04fd9901a999b881b29516733c9b
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-09-06 23:07:28 +0700

    Updating main activity and change app name.

--- app/src/main/java/com/taufik/tixidgithubuser/data/api/Api.kt
+++ app/src/main/java/com/taufik/tixidgithubuser/data/api/Api.kt
@@ -8,7 +8,7 @@ import retrofit2.http.Query
 interface Api {
 
     @GET(UrlEndPoint.USERS_PARAM)
-    @Headers("Authorization: token ${UrlEndPoint.API_KEY}")
+    @Headers("Authorization: token ${UrlEndPoint.API_TOKEN}")
     suspend fun getAllUsers(
         @Query("per_page") perPage: Int
     ): List<UserResponseItem>
--- app/src/main/java/com/taufik/tixidgithubuser/data/api/UrlEndPoint.kt
+++ app/src/main/java/com/taufik/tixidgithubuser/data/api/UrlEndPoint.kt
@@ -2,9 +2,10 @@ package com.taufik.tixidgithubuser.data.api
 
 object UrlEndPoint {
 
+//    const val API_TOKEN = ""
+    const val API_TOKEN = ""
     const val BASE_URL = "https://api.github.com/"
     const val USERS_PARAM = "users"
-    const val API_KEY = ""
     const val STARTING_PAGE_INDEX = 1
     const val PER_PAGE = 10
 }
