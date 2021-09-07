31c9552965bbdaa10facb1cb1c1b77a867724aa3
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-09-07 19:00:28 +0700

    Updating gradle file.

--- app/src/main/java/com/taufik/tixidgithubuser/data/api/UrlEndPoint.kt
+++ app/src/main/java/com/taufik/tixidgithubuser/data/api/UrlEndPoint.kt
@@ -2,7 +2,7 @@ package com.taufik.tixidgithubuser.data.api
 
 object UrlEndPoint {
 
-//    const val API_TOKEN = ""
+//    const val API_TOKEN = "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"
     const val API_TOKEN = ""
     const val BASE_URL = "https://api.github.com/"
     const val USERS_PARAM = "users"
